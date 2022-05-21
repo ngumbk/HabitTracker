b9605bd2f43337114e8e43cf40eb6fde2f3c681c
ngumbk <chulkov.nikita01@gmail.com>
2022-05-22 02:34:28 +0300

    set up 2 xml activities

--- app/src/main/java/com/example/habittracker/MainActivity.java
+++ app/src/main/java/com/example/habittracker/MainActivity.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void profile_button_clicked(View view) {
+        setContentView(R.layout.activity_profile);
+    }
+    public void login_button_clicked(View view) {
+        System.out.println("LOGINLOGINLOGINLOGINLOGINLOGINLOGINLOGINLOGINLOGINLOGIN");
+        Button login_button = findViewById(R.id.login_button);
+        Button logout_button = findViewById(R.id.logout_button);
+        login_button.setVisibility(view.INVISIBLE);
+        logout_button.setVisibility(view.VISIBLE);
+    }
+    public void logout_button_clicked(View view) {
+        System.out.println("LOGUTLOGUTLOGUTLOGUTLOGUTLOGUTLOGUTLOGUTLOGUTLOGUTLOGUT");
+        Button login_button = findViewById(R.id.login_button);
+        Button logout_button = findViewById(R.id.logout_button);
+        login_button.setVisibility(view.VISIBLE);
+        logout_button.setVisibility(view.INVISIBLE);
+    }
+    public void back_to_main_activity_button_clicked(View view) {
         setContentView(R.layout.activity_main);
     }
 
